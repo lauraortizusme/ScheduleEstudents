aa3f0e3a2ab160d542166b7c0910815b8bbdf387
lauraortizusme <125617680+lauraortizusme@users.noreply.github.com>
2025-08-03 08:53:34 -0500

    Update urls.py

--- busyschedule/urls.py
+++ busyschedule/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_schedule, name='schedule'),
-    path('edit/<int:pk>/', views.edit_schedule, name='edit_schedule'),    # NUEVO
-    path('delete/<int:pk>/', views.delete_schedule, name='delete_schedule'),  # NUEVO
+    path('edit/<int:pk>/', views.edit_schedule, name='edit_schedule'),    
+    path('delete/<int:pk>/', views.delete_schedule, name='delete_schedule'),  
 ]
