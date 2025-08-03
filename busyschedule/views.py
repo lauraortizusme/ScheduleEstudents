2399a34096f1f765527241e06a2cc1a3735a9512
Laura <lortizu@eafit.edu.co>
2025-08-03 08:07:56 -0500

    Editar/Eliminar

--- busyschedule/views.py
+++ busyschedule/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ClassScheduleForm
 from .models import ClassSchedule
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 def home(request):
@@ -16,3 +17,21 @@ def add_schedule(request):
     else:
         form = ClassScheduleForm()
     return render(request, 'schedule.html', {'form': form})
+
+def edit_schedule(request, pk):
+    schedule = get_object_or_404(ClassSchedule, pk=pk)
+    if request.method == 'POST':
+        form = ClassScheduleForm(request.POST, instance=schedule)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ClassScheduleForm(instance=schedule)
+    return render(request, 'schedule.html', {'form': form})
+
+def delete_schedule(request, pk):
+    schedule = get_object_or_404(ClassSchedule, pk=pk)
+    if request.method == 'POST':
+        schedule.delete()
+        return redirect('home')
+    return render(request, 'confirm_delete.html', {'schedule': schedule})
